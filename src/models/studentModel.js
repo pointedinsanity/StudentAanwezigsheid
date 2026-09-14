33b0f5bf8732473e8f6e8ad48d23187713308d69
pointedinsanity <mylanroelandts@gmail.com>
2026-09-14 13:11:56 +0200

    Deleted items

--- src/models/studentModel.js
+++
@@ -1,9 +0,0 @@
-const supabase = require('../config/supabaseClient');
-
-async function getAllStudents() {
-  const { data, error } = await supabase.from('students').select('*');
-  if (error) throw error;
-  return data;
-}
-
-module.exports = { getAllStudents };
